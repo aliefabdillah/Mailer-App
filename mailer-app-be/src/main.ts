4dfc98848f4a5e66d82191d101ccdb9cebd015ba
alief abdillah <aliefmabdillah09@gmail.com>
2024-10-07 19:19:26 +0700

    feat: add validation pipe

--- mailer-app-be/src/main.ts
+++ mailer-app-be/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, //if any property request who not in dto will return error
+    }),
+  );
+
   await app.listen(8000);
 }
 bootstrap();
